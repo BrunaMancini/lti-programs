7a09cdf68e9456a9cf1283f43378a499ca981bd2
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-08-29 12:52:33 -0300

    correção

--- api/server.js
+++ api/server.js
@@ -1,4 +1,3 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -6,16 +5,15 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:programId/show': '/:resource/:programId'
-}))
-  
+server.get('/api/beneficiaryLTI/balances', (req, res) => {
+    const db = router.db;
+    const data = db.get('api.beneficiaryLTI.balances').value();
+    res.json(data);
+  });
   
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
