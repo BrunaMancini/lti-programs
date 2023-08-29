e89f4fe65785a2f01f9208650409f7c5d0c2af35
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-08-29 12:51:05 -0300

    correção

--- api/server.js
+++ api/server.js
@@ -6,17 +6,10 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
-server.get('/api/ltiSimulator/summary', (req, res) => {
-    const db = router.db;
-    const data = db.get('api.ltiSimulator.summary').value();
-    res.json(data);
-  });
-
-server.get('/api/ltiSimulator/fullSimulationResult', (req, res) => {
-    const db = router.db;
-    const data = db.get('api.ltiSimulator.fullSimulationResult').value();
-    res.json(data);
-  });
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:programId/show': '/:resource/:programId'
+}))
   
   
 server.use(router)
