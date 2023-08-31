e285473aae7ee9c3b47ff7a4cbf6d88999d45997
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-08-31 18:43:13 -0300

    updating server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
+const sharesRouter = jsonServer.router('shares.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
@@ -10,7 +11,14 @@ server.get('/api/beneficiaryLTI/balances', (req, res) => {
     const data = db.get('api.beneficiaryLTI.balances').value();
     res.json(data);
   });
+
+server.get('/api/sharesPortfolio', (req, res) => {
+    const db = sharesRouter.db;
+    const data = db.get('api.sharesPortfolio').value();
+    res.json(data);
+  });
   
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
